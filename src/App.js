66dc4d35264ff1b6971fe3de5c6dde2a82fbf7f8
Shubhdwivedii <shubhdwivedi101@gmail.com>
2026-03-15 23:32:32 +0530

    new changes

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ import Tag from "./components/Tag";
 function App() {
   return (
     <div className="w-full h-full background flex flex-col items-center">
-      <h1 className="bg-white w-11/12 rounded-lg py-3 px-10 text-center font-bold text-2xl mt-5">RANDOM GIF</h1>
-      <div className="flex flex-col w-full items-center gap-y-10 mt-7">
+      <h1 className="bg-white w-11/12 rounded-lg py-3 px-10 text-center font-bold text-2xl mt-4">RANDOM GIF</h1>
+      <div className="flex flex-col w-full items-center gap-y-8 mt-5">
         <Random/>
         <Tag/>
       </div>
